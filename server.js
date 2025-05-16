0dabef60fa250e0d20ffa128772106d9cbc33151
Smatiii <yankabakov6@gmail.com>
2025-05-16 23:14:36 +0200

    fix bug

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 3000;
 
 db.initDB();
 
+app.get('/', (req, res) => {
+  res.sendFile(join('/home/alex/Project3s/3S/Prototype.html'));
+});
 app.use(cors());
 app.use(express.json());
 
